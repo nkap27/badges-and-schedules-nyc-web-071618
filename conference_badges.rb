cb9c842f100f4c479c10bdacd05963bdec7b919e
Natasha K <kapr.natasha@gmail.com>
2018-07-15 19:31:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,10 +21,13 @@ def assign_rooms(attendees)
 end
 
 def printer
-  batch_badge_creator(attendees).each do |badge|
+  badges = batch_badge_creator(attendees)
+  rooms = assign_rooms(attendees)
+
+  badges.each do |badge|
     puts badge
   end
-  assign_rooms(attendees).each do |attendee|
+  rooms.each do |attendee|
     puts attendee
   end
 end
